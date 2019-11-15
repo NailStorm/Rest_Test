dcf536e8e29c84f3a8b6f5efa9425dcd04e414e9
dmitry <patriot-666@inbox.ru>
2019-11-15 13:49:41 +0300

    Property

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,39 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Описанице, потом напишу
  */
 
 public class Main {
-    public static String HOST = "https://dev.oboz.app/";
-    public static String SECRET = "f17f413f-91ba-473a-b29a-c26b90c3e93b";
-
-    public static String CARRIER_LOGIN = "career1exp1@petrov.gg";
-    public static String CARRIER_PASSWORD = "123456";
-
-    public static String EXPEDITOR_LOGIN = "expeditor1@petrov.gg";
-    public static String EXPEDITOR_PASSWORD = "123456";
-
-    public static String VEHICLE_CLASS_UUID = "";
-    public static String VEHICLE_SPECIE_UUID = "";
-    public static String VEHICLE_TYPE_UUID = "";
-
-    public static String HUMAN_CLASS_UUID = "";
-    public static String HUMAN_SPECIE_UUID = "";
-    public static String HUMAN_TYPE_UUID = "";
+    public Main() throws FileNotFoundException {
+    }
 
     public static void main(String[] args) throws IOException {
         HttpTools okhttp = new HttpTools();
         SystemTools tool = new SystemTools();
 
-
+        FileInputStream config =
+                new FileInputStream("src/main/resources/config.properties");
+        Properties property = new Properties();
+        property.load(config);
 
         /**
          * Создаем ресурс
          */
         okhttp.createResource(
-                okhttp.userToken(HOST, SECRET, CARRIER_LOGIN, CARRIER_PASSWORD),
-                VEHICLE_CLASS_UUID, VEHICLE_SPECIE_UUID, VEHICLE_TYPE_UUID);
+                okhttp.userToken(
+                        property.getProperty("host"),
+                        property.getProperty("secret"),
+                        property.getProperty("carrier.login"),
+                        property.getProperty("carrier.password")
+                ),
+                property.getProperty("vehicle.class.uuid"),
+                property.getProperty("vehicle.specie.uuid"),
+                property.getProperty("vehicle.type.uuid")
+        );
 
         //прост юид
         System.out.println(tool.uuidGenerator());
